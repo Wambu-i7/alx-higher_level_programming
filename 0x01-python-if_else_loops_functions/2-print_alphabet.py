6baaa88c6a716317b3d0c34a0c12d73f931bb60c
Wambu-i7 <wambuindikiru@gmail.com>
2023-11-29 03:56:55 -0800

    alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 start_alph = ord('a')
 end_alph = ord('z')
-
 for alphabets in range(start_alph, end_alph + 1):
-    print(chr(alphabets), end='')
+    print("{}".format(chr(alphabets)), end='')
