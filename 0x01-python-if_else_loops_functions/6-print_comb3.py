5607abfaf109f6f14e8979a7456aaaf3b82dceda
Wambu-i7 <wambuindikiru@gmail.com>
2023-11-29 05:28:32 -0800

    Possible combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-combo = []
 for tens in range(10):
     for ones in range(tens + 1, 10):
-        combo.append("{:d}{:d}".format(tens, ones))
-result = ", ".join(combo)
-print(result)
+        print("{:d}{:d}".format(tens, ones), end=", ")
+print()
