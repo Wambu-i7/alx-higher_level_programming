3e3100e290041330dd91f38ac1526367a319ef0d
Wambu-i7 <wambuindikiru@gmail.com>
2023-11-30 00:50:43 -0800

    Alternate lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for c in reversed(range(ord('a'), ord('z') + 1)):
-    print("{}".format(chr(c) if c % 2 == 0 else chr(c + ord('A') - ord('a'))), end='')
+    print("{}".format(chr(c) if c % 2 == 0 else chr(c + ord('A')
+        - ord('a'))), end='')
 print()
