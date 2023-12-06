9e77eb2bfc668134e15fbddaf04b16510fa6fa30
Wambu-i7 <wambuindikiru@gmail.com>
2023-12-06 02:35:27 -0800

    Add command args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-import math
 if __name__ == "__main__":
-n = len(argv) - 1
-Sum = 0
-for i in range(1, n + 1):
-    Sum += int(argv[i])
+    from sys import argv
+    import math
+    n = len(argv) - 1
+    Sum = 0
+    for i in range(1, n + 1):
+        Sum += int(argv[i])
      
-print("{}".format(Sum))
+    print("{}".format(Sum))
