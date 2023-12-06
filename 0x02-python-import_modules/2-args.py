9f884e0ac60a47becbe6ba6da4684624d45beaa8
Wambu-i7 <wambuindikiru@gmail.com>
2023-12-06 01:10:16 -0800

    Command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    num_args = len(argv) - 1
-    print("{} argument{}:".format(num_args, 's' if num_args != 1 else '') if num_args != 0 else '0 arguments.')
-    if num_args > 0:
-        print(end="")
-        for i in range(1, num_args + 1):
-            print("\n{}: {}".format(i, argv[i]), end="")
-        print()
+    n = len(argv) - 1
+    if n < 1:
+        print("{} arguments.".format(n), end=" ")
+    elif n == 1:
+        print("{} argument:".format(n), end=" ")
+    else:
+        print("{} arguments:".format(n), end=" ")
+    for i in range(1, n + 1):
+        print("\n{}: {}".format(i, argv[i]), end=" ")
+    print()
