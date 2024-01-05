e00283eda87f67c949f83b8d03b42cd6183d4cfa
Wambu-i7 <wambuindikiru@gmail.com>
2024-01-05 04:36:04 -0800

    Square initialized

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is the documentation for my_module.
+
+It provides a brief overview of the module's purpose and functionality.
+"""
+
+
+class Square:
+    """A class that represents a square."""
+    def __init__(self, size):
+        """Initialize a Square instance.
+
+        Args:
+            size: size of the square.
+        """
+
+        self.__size = size
