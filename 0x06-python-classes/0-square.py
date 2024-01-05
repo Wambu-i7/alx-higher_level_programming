2bfcad519979d60865292403eb452510b0bdec86
Wambu-i7 <wambuindikiru@gmail.com>
2024-01-05 04:24:30 -0800

    Classes and objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is the documentation for my_module.
+
+It provides a brief overview of the module's purpose and functionality.
+"""
+class Square:
+    """A class that represents a square."""
+    pass
