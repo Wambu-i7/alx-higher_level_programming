ce98b1aed4543c74635e2cd46ed4916fac43a597
Wambu-i7 <wambuindikiru@gmail.com>
2024-01-06 08:17:12 -0800

    Define a class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,6 +2,8 @@
 """This is a brief documentation of my module
 It provides a brief overview of the module's purpose and functionality.
 """
+
+
 class Rectangle:
     """A class that represent a rectangle"""
     pass
