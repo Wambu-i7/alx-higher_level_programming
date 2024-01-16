e754b93360bde7d8443519348a5175bcdbd78ad8
Wambu-i7 <wambuindikiru@gmail.com>
2024-01-16 06:58:25 -0800

    Base geo

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,42 +1,25 @@
 #!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
 
-"""An empty class"""
 
-
-class BaseGeometry(object):
-    """BaseGeometry is a class that serves as a template
-    for creating custom objects.
-    Public Methods:
-    - area(self): Raises an exception indicating that the
-    area() method is not implemented.
-    """
+class BaseGeometry:
+    """Reprsent base geometry."""
 
     def area(self):
-        """
-        area(self):
-
-        Raises an exception that the area() method is not implemented.
-        """
-
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        integer_validator(self, name, value)
+        """Validate a parameter as an integer.
 
-        Validates that the value is an integer greater than 0.
-
-
-        Parameters:
-        - name (str): The name of the variable being validated.
-        - value: The value to be validated.
-
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
         Raises:
-        - TypeError: If the value is not an integer.
-        - ValueError: If the value is less than or equal to 0.
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
