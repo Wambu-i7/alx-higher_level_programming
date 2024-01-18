8b7e8d770b2e392fa7673ee626c9c8f8fc9f8be7
Wambu-i7 <wambuindikiru@gmail.com>
2024-01-18 04:41:13 -0800

    Read a text

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,6 @@
 
 
 def read_file(filename=""):
+    """ Reads the content of a text file (UTF-8) and prints it to stdout."""
     with open(filename, mode='r', encoding='utf-8') as rfile:
         print(rfile.read())
